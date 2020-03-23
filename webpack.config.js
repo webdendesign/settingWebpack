8fe366996cb9ebe2514c6fe3204a504fd0f1d4ec
Den <fabri.din@yandex.ru>
2020-03-23 11:40:23 +0500

    setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+
+module.exports = {
+    mode: "development",
+
+    module: {
+        rules: [
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            // Loading images
+            {
+                test: /\.(jpg|png|jpeg|gif|ico)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images',
+                        name: '[name]-[sha1:hash:7].[ext]'
+                    }
+                }]
+            },
+            // Loading fonts
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts',
+                        name: '[name].[ext]'
+                    }
+                }]
+            },
+            // Loading CSS
+            {
+                test: /\.(css)$/,
+                use: ['style-loader', 'css-loader']
+            },
+            // Loading SASS/SCSS
+            {
+                test: /\.(s[ca]ss)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+
+        ]
+    }
+};
